b4bd6b0305679ab1079c99779591b0565795ff86
Aaron <apowell829@gmail.com>
2019-01-30 21:36:30 +0000

    data passing react classes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+class IndecisionApp extends React.Component {
+    render() {
+        const title = "Indecision";
+        const subtitle = "Can't decide? Computerise.."
+        let options = ["1", "2", "g"];
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle} />
+                <Options options={options} />
+                <AddOption />
+                <Action />
+            </div>
+        )
+    }
+}
+
+
 class Header extends React.Component {
     render() {
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Can't decide? Computerise..</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         )
     }
@@ -11,22 +28,44 @@ class Header extends React.Component {
 
 
 class Action extends React.Component {
+    handlePick() {
+        alert("Handle Pick")
+    }
     render() {
         return (
             <div>
-                <button>What should I do?</button>
+                <button onClick={this.handlePick}>What should I do?</button>
             </div>
         )
     }
 }
 
 class Options extends React.Component {
+    handleRemoveAll() {
+        alert("Remove all")
+    }
     render() {
         return (
             <div>
-                <h1>Options here</h1>
+                {
+                    this.props.options.map((option) => {
+                        return <Option key={option} optionText={option} />
+                    })
+                }
+                <Option />
+                <button onClick={this.handleRemoveAll}>Remove All</button>
             </div>
         );
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.optionText}
+            </div>
+        )
     }
 }
 class AddOption extends React.Component {
@@ -38,13 +77,5 @@ class AddOption extends React.Component {
         )
     }
 }
-const jsx = (
-    <div>
-        <Header />
-        <Action />
-        <Options />
-        <AddOption />
-    </div>
-)
 
-ReactDOM.render(jsx, document.querySelector("#app"));
+ReactDOM.render(<IndecisionApp />, document.querySelector("#app"));
