d84c87a7cc19a0329c1ee4cb7c01bb2a0da49292
Aaron <apowell829@gmail.com>
2019-01-31 00:43:15 +0000

    added counter component

--- src/build-it-vis.js
+++ src/build-it-vis.js
@@ -1,50 +1,15 @@
-class Person {
-    constructor(name = "Anon", age = 0) {
-        this.name = name;
-        this.age = age
-    }
-    getGreeting() {
-        return `Hello I am ${this.name}`
-    }
-    getDesc() {
-        return `${this.name} is ${this.age} year${this.age === 1 ? `` : `s`} old.`
-    }
-}
-
-class Student extends Person {
-    constructor(name, age, className) {
-        super();
-        this.className = className;
-    }
-    hasMajor() {
-        return !!this.className
-    }
-    getDesc() {
-        let description = super.getDesc();
-
-        if (this.hasMajor()) {
-            description += ` Their major is ${this.className}`
-        }
-        return description
+class Counter extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Count: </h1>
+                <button>+1</button>
+                <button>-1</button>
+                <button>reset</button>
+            </div>
+        );
     }
 }
 
-class Traveller extends Person {
-    constructor(name, age, homeLocation) {
-        super();
-        this.homeLocation = homeLocation;
-    }
-    getGreeting() {
-        let greet = super.getGreeting();
-        if (this.homeLocation) {
-            greet += `, I am visiting from ${this.homeLocation}`
-        }
-        return greet;
-    }
-}
 
-const me = new Student("aaron", 20, "cs")
-const me1 = new Traveller("aaron", 20)
-
-console.log(me.getGreeting());
-console.log(me1.getGreeting());
+ReactDOM.render(<Counter />, document.getElementById("app"));
